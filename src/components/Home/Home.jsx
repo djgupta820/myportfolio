d688612cd45098d23e91e83f064469d84a69ee16
Dheeraj Gupta <djgupta820@gmail.com>
2024-02-15 17:11:43 +0530

    frontend improved

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="container">
       <div className="image">
-        <h1>Full Stack Developer</h1>
-        <h1>Hi I'm Dheeraj Gupta</h1>
+        <div className="img-txt">
+          <h1>Full Stack Developer</h1>
+          <h1>Hi I'm Dheeraj Gupta</h1>
+        </div>
         <div className="btns">
-          <a href="/Full_Stack_Dheeraj" download>Download Resume</a>
+          <a href="/Full_Stack_Dheeraj" download>
+            Download Resume
+          </a>
           <Link to="/contact">Contact</Link>
         </div>
       </div>
