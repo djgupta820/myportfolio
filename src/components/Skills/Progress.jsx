1a3946facaa2c488fb774cbe5ecc176d11ccf4f1
Dheeraj Gupta <djgupta820@gmail.com>
2024-02-16 19:06:31 +0530

    some changes for skills

--- src/components/Skills/Progress.jsx
+++ src/components/Skills/Progress.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Progress = (props) => {
   return (
-    <div className="progressbar" title={props.data * 10 + "%"} >
+    <div className="progressbar" title={props.data * 10 + "%"}>
       <div className="progress" style={{ width: `${props.data * 10}%` }}>
         {props.data}0%
       </div>
--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Progress from "./Progress";
 
 const Skill = (props) => {
